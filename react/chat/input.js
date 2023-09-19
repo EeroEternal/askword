9aa0becf68ebc86d125cebe0af4d34570f92565d
Eero <eeroeternal@gmail.com>
2023-09-19 09:03:16 +0800

    fix input

--- react/chat/input.js
+++ react/chat/input.js
@@ -12,15 +12,29 @@ export default function Input() {
     }
   }
 
+  function handleKeyUp(event) {
+    if (event.key === 'Backspace') {
+      // Handle desired action here for Delete key press
+      console.log('Backspace key pressed')
+      const textareaValue = event.target.value;
+      console.log("textareaValue", textareaValue.length)
+      if (textareaValue.length === 0) {
+        setHeight(30);
+      }
+    }
+  }
+
   return (
 
     <div className={input_css}>
-      <textarea style={{ height: `${height}px`, resize: "none", overflowY: "hidden" }} onKeyDown={handleKeyDown} className={textarea_css} placeholder="请输入..." spellCheck="false" rows="1" />
+      <textarea style={{ height: `${height}px`, resize: "none", overflowY: "hidden" }} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} className={textarea_css} placeholder="请输入..." spellCheck="false" rows="1" />
+
       <button type="submit" className="ml-4 flex items-center justify-center  text-gray-300 hover:text-gray-600 font-bold py-2 px-4 rounded-full">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>
       </button>
+
     </div>
   )
 }
